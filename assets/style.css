1eccc5b9a0f6a5afe6efaa9fc1664da69a9b6f3f
John Baxter <juancho420@ymail.com>
2021-03-23 09:57:12 -0700

    framework for professional portfolio

---
+++ assets/style.css
@@ -0,0 +1,19 @@
+* {
+    background-color: lightyellow;
+}
+
+.About {
+    background-color: darkblue;
+}
+
+.Work {
+    background-color: blue;
+}
+
+.Contact {
+    background-color: dodgerblue;
+}
+
+.Resume {  
+    background-color: lightseagreen;
+}
